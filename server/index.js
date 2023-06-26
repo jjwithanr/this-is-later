6a86c014baead591f3cbceb19598683eb2710d3f
jjchang623@yahoo.com <jrchang@jrchang180480.local>
2023-06-26 16:03:54 -0700

    Retrieve Pocket recent saves

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const port = 3000;
 
 require('dotenv').config();
 const consumerKey = process.env.POCKET_CONSUMER_KEY;
-console.log(consumerKey);
 const redirectUri = 'http://localhost:3000/pocket';
 
 const headers = {
@@ -27,7 +26,7 @@ app.get('/', (req, res) => {
       res.redirect(redirectUrl);
     })
     .catch(error => {
-      // console.error(error);
+      console.error(error);
       res.send('An error occurred while obtaining a request token');
     });
 });
@@ -50,11 +49,34 @@ app.get('/pocket', (req, res) => {
       axios.post(retrieveUrl, data, { headers: headers })
         .then(response => {
           const items = response.data.list;
-          const html = '<h1>Pocket</h1>' + '<ul>' + Object.values(items).map(item => `<li><p>${item.given_url}</p></li>`).join('') + '</ul>';
+
+          const imageElements = [];
+          // Loop through each saved link and create the associated HTML elements
+          Object.keys(items).forEach(key => {
+            const item = items[key];
+            let imgUrl = 'https://cdn.worldvectorlogo.com/logos/pocket.svg';
+            if (item.has_image == '1') {
+              imgUrl = item.top_image_url;
+            }
+            const title = item.given_title;
+            const linkUrl = item.resolved_url;
+
+            // Create the HTML for the link's image and link element
+            const imgElement = `<img src="${imgUrl}" width=150>`;
+            const linkElement = `<a href="${linkUrl}" target="_blank">${imgElement}</a><p>${title}</p>`;
+
+            // Add the link's HTML to the array
+            imageElements.push(linkElement);
+          });
+
+          // Send the HTML for all link images as a response
+          const html = imageElements.join('\n');
           res.send(html);
+          // const html = '<h1>Pocket</h1>' + '<ul>' + Object.values(items).map(item => `<li><p>${item.given_url}</p></li>`).join('') + '</ul>';
+          // res.send(html);
         })
         .catch(error => {
-          // console.error(error);
+          console.error(error);
           res.send('An error occurred while retrieving your saves');
         });
     })
